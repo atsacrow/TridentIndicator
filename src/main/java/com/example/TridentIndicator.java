34a0931334a1e2e83b349441ee8d8a8acb4639f7
atsacrow <atsacrow@gmail.com>
2026-08-27 11:52:23 +0500

    Update TridentIndicator.java

--- src/main/java/com/example/TridentIndicator.java
+++ src/main/java/com/example/TridentIndicator.java
@@ -2,7 +2,7 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
